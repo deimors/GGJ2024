5f5d9ab5313481d74383f5da40151eb7b65f716f
Brad Rhodes <1196817+bradrhodes@users.noreply.github.com>
2024-01-28 11:55:36 -0600

    Added random enemy spawns but they're causing weirdness

--- Assets/Game/Implementation/Presentation/EnemiesInitializer.cs
+++ Assets/Game/Implementation/Presentation/EnemiesInitializer.cs
@@ -1,14 +1,19 @@
+using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 public class EnemiesInitializer : MonoBehaviour
 {
 	[Inject] public IEnemyCommands EnemyCommands { private get; set; }
+	public int NumberofEnemies;
 
 	void Start()
 	{
 		var spawnPoints = GetComponentsInChildren<EnemySpawnPoint>();
+		// var spawnPoints = GetRandomSpawnPoints();
 
 		var enemyPositions = spawnPoints
 			.ToDictionary(
@@ -20,5 +25,30 @@ public class EnemiesInitializer : MonoBehaviour
 			spawnPoint.gameObject.SetActive(false);
 
 		EnemyCommands.Initialize(new EnemiesConfig(enemyPositions));
+	}
+
+	private IEnumerable<EnemySpawnPoint> GetRandomSpawnPoints()
+	{
+		var spawnPoints = GetComponentsInChildren<EnemySpawnPoint>();
+		var spawnPointsUpperBound = spawnPoints.Length - 1;
+
+		var randomSpawnIndexList = new List<int>();
+		for (var i = 0; i < NumberofEnemies; i++)
+		{
+			var newSpawnPointFound = false;
+			while (!newSpawnPointFound)
+			{
+				var possibleSpawnPoint = Random.Range(0, spawnPointsUpperBound);
+				if (randomSpawnIndexList.Contains(possibleSpawnPoint)) continue;
+				
+				randomSpawnIndexList.Add(possibleSpawnPoint);
+				newSpawnPointFound = true;
+			}
+		}
+
+		foreach (var spawnIndex in randomSpawnIndexList)
+		{
+			yield return spawnPoints[spawnIndex];
+		}
 	}
 }
